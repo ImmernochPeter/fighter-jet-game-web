fe002044cb665525143f5c80422ada332d71f3db
Peter Koban <peter.koban@tu-dresden.de>
2023-04-19 09:50:21 +0200

    feat(setup): add url files and index-view

---
+++ jetgame/home/urls.py
@@ -0,0 +1,10 @@
+"""
+Urls
+"""
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index-home'),
+]
--- jetgame/home/views.py
+++ jetgame/home/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
+from django.views import View
 
 # Create your views here.
+
+class IndexView(View):
+    """
+    IndexView
+    """
+
+    def get(self,request:HttpRequest)->HttpResponse:
+        """
+        get
+        """
+        return render(request,"home/index_home.html")
